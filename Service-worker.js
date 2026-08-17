4160e8fa34355b3d3d7ebb61a6103146f912f3d3
p9fgp2kkms-web <p9fgp2kkms@privaterelay.appleid.com>
2026-08-17 21:36:39 +0100

    Update Service-worker.js

--- Service-worker.js
+++ Service-worker.js
@@ -1,23 +1,37 @@
-const CACHE_NAME = "hamani-coiffeur-v1";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE_NAME = "hamani-coiffeur-v2";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES);
-    })
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
